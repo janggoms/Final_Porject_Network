9d1533e6ec295253094c53752f924048f71d670d
hyunhyeee <gusgp0629@naver.com>
2023-11-28 01:53:18 +0900

    각 클래스마다 소켓 연결 - 현혜

--- Final_Project_Network/src/_03GameHost.java
+++ Final_Project_Network/src/_03GameHost.java
@@ -13,7 +13,6 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.net.SocketException;
 import java.util.ArrayList;
 import java.util.List;
 
